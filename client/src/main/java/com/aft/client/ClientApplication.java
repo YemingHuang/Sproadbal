b1e2e49a4f3449c233d617457d09c2c00c90fece
yhuang <yhuang@appliedfinancialtechnology.com>
2018-01-15 11:10:54 +0000

    Gets Ribbon on, for real.

--- client/src/main/java/com/aft/client/ClientApplication.java
+++ client/src/main/java/com/aft/client/ClientApplication.java
@@ -3,6 +3,7 @@ package com.aft.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,10 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
-@RibbonClient(name = "client", configuration = ClientConfiguration.class)
+@RibbonClient(name = "say-hello", configuration = ClientConfiguration.class)
 public class ClientApplication {
 
+    @LoadBalanced
     @Bean
     RestTemplate restTemplate(){
         return new RestTemplate();
@@ -25,7 +27,7 @@ public class ClientApplication {
 
     @RequestMapping("/hi")
     public String hi(@RequestParam(value="name", defaultValue="Gilgamesh") String name) {
-        String greeting = this.resttTemplate.getForObject("http://localhost:8090/greeting", String.class);
+        String greeting = this.resttTemplate.getForObject("http://say-hello/greeting", String.class);
         return String.format("%s, %s!", greeting, name);
     }
 
